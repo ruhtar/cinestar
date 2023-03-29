7ae0ad1119be62a95a72c4f7ef9abbf47ca0058f
Arthur Amorim <arthur.amorim@inventione.com.br>
2023-03-29 20:29:10 -0300

    validators

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,5 +7,42 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
-  @Input() btnText!: string
+  @Input() btnText!: string;
+
+  cinemaForm!: FormGroup
+
+
+  constructor(){
+  }
+
+  ngOnInit(){
+    this.cinemaForm = new FormGroup({
+      id: new FormControl(""),
+      ownerName: new FormControl("", [Validators.required]),
+      cinemaName: new FormControl("", [Validators.required]),
+      street: new FormControl("", [Validators.required]),
+      cpf: new FormControl("", [Validators.required]),
+      description: new FormControl("")
+    });
+  }
+
+  get ownerNameControlname(){
+    return this.cinemaForm.get("ownerNameControlname")!;
+  }
+  
+  get cpfControlname(){
+    return this.cinemaForm.get("cpfControlname")!;
+  }
+
+  get cinemaNameControlName(){
+    return this.cinemaForm.get("cinemaNameControlName")!;
+  }  
+  
+  get streetControlName(){
+    return this.cinemaForm.get("streetControlName")!;
+  }
+  
+  submit(){
+    console.log("enviou");
+  }
 }
---
+++ src/app/interfaces/Cinema.ts
@@ -0,0 +1,9 @@
+//Entidades
+export interface Cinema {
+    id?: number, //opcional
+    ownerName: string,
+    cpf: string,
+    cinemaName: string,
+    street: string,
+    description?: string,
+}
