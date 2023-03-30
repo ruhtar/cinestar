7a5133e0b400db6e1238254877ec6866162927dd
ruhtar <arthur.amorim10@gmail.com>
2023-03-29 21:22:01 -0300

    backend

---
+++ api/api/Models/Cinema.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Models
+{
+  public class Cinema
+  {
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public string OwnerName { get; set; }
+    [Required]
+    public string CPF { get; set; }
+    [Required]
+    public string CinemaName { get; set; }
+    [Required]
+    public string Street { get; set; }
+    public string Description { get; set; }
+  }
+}
