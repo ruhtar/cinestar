0edf3ace98ade4b5fdd245581c86d05c419bd884
ruhtar <arthur.amorim10@gmail.com>
2023-04-11 00:01:38 -0300

    home and animations

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -12,9 +12,10 @@ import { ObservableService } from 'src/app/services/observable.service';
 })
 export class HomeComponent{
   images = [
-    { image: 'https://via.placeholder.com/600x400', thumbImage: 'https://via.placeholder.com/150x100' },
-    { image: 'https://via.placeholder.com/600x400', thumbImage: 'https://via.placeholder.com/150x100' },
-    { image: 'https://via.placeholder.com/600x400', thumbImage: 'https://via.placeholder.com/150x100' },
-    { image: 'https://via.placeholder.com/600x400', thumbImage: 'https://via.placeholder.com/150x100' }
+    { image: 'https://canalcienciascriminais.com.br/wp-content/uploads/2019/05/cinema.png', thumbImage: 'https://canalcienciascriminais.com.br/wp-content/uploads/2019/05/cinema.png' },
+    { image: 'https://www.acessa.com/_midias/jpg/2023/02/08/800x533/1_20220914_sala_de_cinema-150861.jpeg', thumbImage: 'https://www.acessa.com/_midias/jpg/2023/02/08/800x533/1_20220914_sala_de_cinema-150861.jpeg' },
+    { image: 'https://cdn.ibahia.com/wp-content/uploads/2022/10/cinema.png', thumbImage: 'https://cdn.ibahia.com/wp-content/uploads/2022/10/cinema.png' },
+    { image: 'https://muralzinhodeideias.com.br/wp-content/uploads/2023/02/cinema-divulgacao.jpeg', thumbImage: 'https://muralzinhodeideias.com.br/wp-content/uploads/2023/02/cinema-divulgacao.jpeg' },
+    { image: 'https://www.carlosmagno.com.br/fotos_do_sistema/Cinema_Pipoca.jpg', thumbImage: 'https://www.carlosmagno.com.br/fotos_do_sistema/Cinema_Pipoca.jpg' }
   ];
 }
